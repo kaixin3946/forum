ca219ff054e17a76d07826b525e4d55b7cd45f58
Lily49193 <714549865@qq.com>
2017-05-15 10:20:41 +0800

    发表文章，查看文流程跑通

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
  */
 var express=require("express");
 var app=express();
-var db=require("./model/db.js");
-var md5=require("./model/md5.js");
+//var db=require("./model/db.js");
+//var md5=require("./model/md5.js");
 var session = require('express-session');
+var router=require("./router");
 
 //使用session
 app.use(session({
@@ -17,121 +18,26 @@ app.use(session({
 app.set("view engine","ejs");
 app.use(express.static('./public'));
 
-app.get("/",function(req,res){
-    res.render("index",{layout:"nav",page:"首页","username":req.session.username});
-});
-
-app.get("/index",function(req,res){
-    res.render("index",{layout:"nav",page:"首页","username":req.session.username});
-});
-
-
-app.get("/login",function(req,res){
-    db.find("userpwd",{},function(err,result){
-        res.render("login",{
-            layout:"nav",
-            userList:result,
-            "username":req.session.username
-        });
-    })
-});
-app.get("/userlogin",function(req,res){                            /* 登录 */
-    //从数据库提取
-    var username=req.query.username;
-    var json1=[{"username":req.query.username,"pwd":req.query.pwd}];
-
-    db.find("userpwd",{"username":req.query.username},function(err,result){
-
-        if(err) {
-            console.log("查询出错");
-        };
-
-        console.log(result);
-
-        if(result.length==0){
-            console.log("用户名不存在");
-            res.send({"result":0});
-        }else{
-            if(result[0].pwd==req.query.pwd){
-                console.log("匹配成功，可以登录");
-                req.session.login="1";
-                req.session.username=username;
-                res.send({"result":1,"username":username});
-            }else{
-                console.log("用户名不存在或密码错误");
-                res.send({"result":-1});
-            }
-        }
-    })
-});
-
-app.get("/userregist",function(req,res){                /* 注册 */
-    //这是要存入数据库
-    var pwd=req.query.pwd;
-    var username=req.query.username;
-    pwd=md5(md5(pwd)+2);
-    var json1=[{"username":username,"pwd":pwd}];
-
-    db.find("userpwd",{"username":req.query.username},function(err,result){
-
-        if(err) {
-            console.log("查询出错");
-        };
-
-        //console.log("result:"+result);
-        if(result.length==0){
-            db.insertOne("userpwd",json1,function(err,result){              //这个json1是json数组
-                if(err) {
-                    //console.log("没有存入数据库");
-                    res.send({"result":-1});
-                };
-
-                //console.log("已经存入数据库");
-                req.session.login="1";
-                req.session.username=username;
-                res.send({"result":1,"username":username});
-            })
-        }else{
-            res.send({"result":0});
-        }
-    })
-});
-
-app.get("/outLogin",function(req,res){                            /* 退出登录 */
-    //从数据库提取
-    req.session.login="0";
-    req.session.username=null;
-    res.send({success:true});
-});
+app.get(/^\/index$|(^\/$)/,router.showIndex);                      //显示首页
+app.get("/login",router.showLogin);                                 //显示登录页面，给出列表
+app.get("/userlogin",router.doLogin);                                //操作：用户登录
+app.get("/userregist",router.doRegister);                          //操作：用户注册
+app.get("/outLogin",router.doOutLogin);                           //操作：退出登录
+app.get("/social/list",router.getSocialList);                         //操作：获取信息社会板块列表
+app.get("/articleList",router.getArticleList);                        //操作：获取各个板块的文章列表
+app.get("/articleContent",router.showArticleContent);          //显示文章架构
+app.get("/getArticleContent",router.getArticleContent);          //获取文章内容
+app.get("/newTopic",router.showNewTopic);                    //显示新话题页面
+app.get("/social/newTopic",router.doNewTopic);             //操作：提交新话题内容
+app.get("/social/articleList",router.getSocialArticleList);   //获取信息社会某一板块下的文章列表
+app.post("/social/reply",router.doNewReply);             //操作：提交新话题内容
+/*app.get("/articleContent",router.showArticleContent);*/
 
 
-app.get("/social/list",function(req,res){                            /* 信息社会  social/list*/
-    //从数据库提取
 
-    var blockNum=parseInt(req.query.blockNum);
-    db.find("social",{"blockNum":blockNum},function(err,result){
-
-        if(err) {
-            console.log("查询出错");
-        };
-        console.log(result);
-
-        if(result.length==0){
-            res.send({"result":0});
-        }else{
-            console.log("返回");
-            res.send({"result":result[0]});
-        }
-    })
-});
-app.get("/articleList",function(req,res){                                               /*  这个接口是有先后关系的,不然上一个返回来不能进入这个页面里 */
-    res.render("articleList",{layout:"nav","username":req.session.username,page:"文章列表"});
-});
-
-app.get("/visit",function(req,res){
-    res.render("visit",{page:"浏览","username":req.session.username});
-});
-
+//app.get("/visit",function(req,res){
+//    res.render("visit",{page:"浏览","username":req.session.username});
+//});
 
 app.listen("3000",function(){
     console.log("打开3000端口");
--- model/db.js
+++ model/db.js
@@ -5,47 +5,51 @@ var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
 
-/* 连接数据库 */
-function _connect(callback){
-    // Connection URL
+// 连接数据库
+function _connect(){
     var url = 'mongodb://localhost:27017/userpwd';
 
     // Use connect method to connect to the Server
-    MongoClient.connect(url, function(err, db) {
-        //assert.equal(null, err);
-        if(err){
-            callback(err,null)
-        }else{
-            //console.log("Connected correctly to server");
-            callback(err, db);
-        }
-
-        //db.close();
+    var promise=new Promise(function(resolve,reject){
+        MongoClient.connect(url,function(err,db){
+            if(err){
+                reject(err);
+            }else{
+                resolve(db);
+            }
+        })
     });
+    return promise;
 }
 
-exports.insertOne=function(collectionName,json,callback){                   //可以是json 数组
-    _connect(function(err,db){
+exports.find=function(collectionName,json,args,callback){                            //查找
+    //  args={"pageth":1,"pageNums":10}
+    var result=[];
+    if(arguments.length==3){
+        callback=args;
+        args={"pageth":1,"pageNums":0,"sort":{}};
+    }else if(arguments.length==2){
+        callback=json;
+        args={"pageth":1,"pageNums":0,"sort":{}};
+        json={};
+    }else if(arguments.length==4){
+        var a=args.pageth||1;
+        var b=args.pageNums||0;
+        var c=args.sort||{};
+
+        args={"pageth":a,"pageNums":b,"sort":c};
+
+    }else if(arguments.length==1){
+        throw new Error("参数传入错误");
+    }
+
+    //console.log("collectionName:"+collectionName);
+    _connect().then((db)=>{
+        //console.log(db);
         var collection = db.collection(collectionName);
+        var cur=collection.find(json).skip((args.pageth-1)*args.pageNums).limit(args.pageNums).sort(args.sort);
 
-        collection.insertMany(json, function(err, result) {
-            //assert.equal(err, null);
-            console.log("Inserted 1 documents into the document collection");
-
-            callback(err, result);
-            db.close(); //关闭数据库
-        });
-    });
-
-};
-
-exports.find=function(collectionName,json,callback){
-    _connect(function(err,db){
-        var collection = db.collection(collectionName);
-        var cur=collection.find(json);
-        var result=[];
-
-        cur.each(function(err,doc){
+        cur.each((err,doc)=>{
             if (err) {
                 callback(err, null);
                 db.close(); //关闭数据库
@@ -59,9 +63,73 @@ exports.find=function(collectionName,json,callback){
                 db.close(); //关闭数据库
             }
         })
-
-
+    },(err,db)=>{
+        console.log("到then没成功");
+        throw  err;
     });
+};
+
+exports.insertOne=function(collectionName,json,callback){                   //可以是json 数组
+     _connect().then((db)=>{
+             var collection = db.collection(collectionName);
+             var promise=new Promise(function(resolve,reject){
+                 collection.insertOne(json, function(err, result) {
+                     if(err){
+                         reject(err);
+                     }else{
+                         resolve(result);
+                     }
+                 });
+             })
+
+            return promise;
+     },(err)=>{
+         console.log("插入失败:"+err);
+     }).then((result)=>{
+         callback(err, result);
+         console.log("Inserted 1 documents into the document collection");
+         db.close(); //关闭数据库
+     },(err)=>{
+         console.log("插入失败:"+err);
+     });
+
+};
+
+exports.updateOne=function(collectionName,jsonSrc,jsonDest,callback){
+    _connect().then((db)=>{
+            var collection = db.collection(collectionName);
+            var promise=new Promise(function(resolve,reject){
+                collection.updateOne(jsonSrc, { $set: jsonDest }, function(err, result) {
+                    if(err){
+                        reject(err);
+                    }else{
+                        resolve(result);
+                    }
+                });
+            })
+            return promise;
+    },(err)=>{
+        console.log("更新出错啦："+err)
+    }).then((result)=>{
+        console.log("Updated");
+        callback(null,result);
+        db.close(); //关闭数据库
+    },(err)=>{
+        console.log("更新出错啦："+err)
+    });
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
